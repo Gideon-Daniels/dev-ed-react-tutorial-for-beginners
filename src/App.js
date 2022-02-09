c5e89e6ad1a819bc1083ad5fd92495b3ab08b93a
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2022-02-09 11:21:09 +0200

    Finished Tutorial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Tweet from "./Tweet";
 
 function App() {
-  return;
-  <div></div>;
+  const [users, setUsers] = useState([
+    { name: "Gideon", message: "Hello there" },
+    { name: "Dragneelix", message: "I am a dragon" },
+    { name: "Dragoonix", message: "I am the strogest creature in the world" },
+    { name: "Igneelix", message: "I am a fairy dragon " },
+  ]);
+
+  return (
+    <div className="app">
+      {users.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
